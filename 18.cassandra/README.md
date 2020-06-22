9143f94897bee07b57ee47989141a465b331c8ea
arunpa0206 <mail_arunn@yahoo.com>
2020-06-22 17:33:01 +0530

    changes made to cassandra

--- 18.cassandra/README.md
+++ 18.cassandra/README.md
@@ -1,3 +1,5 @@
 Create a Casandra cluster usingh the following and use the above code to connect
 
 https://www.instaclustr.com/support/documentation/getting-started/connect-cassandra-golang-sample-code/
+
+after creating a cluster set cidr range to 0.0.0.0./0 for public access
--- 18.cassandra/main.go
+++ 18.cassandra/main.go
@@ -11,10 +11,15 @@ import (
 func main() {
 	// connect to the cluster
 	cluster := gocql.NewCluster("44.233.14.94", "52.37.53.85", "44.230.43.174") //replace PublicIP with the IP addresses used by your cluster.
+	//set consistency level to quorum mode
 	cluster.Consistency = gocql.Quorum
+	//set the protocol version
 	cluster.ProtoVersion = 4
+	//set the timeout for cassandra connection
 	cluster.ConnectTimeout = time.Second * 10
+	//authenticate the enter cassandra
 	cluster.Authenticator = gocql.PasswordAuthenticator{Username: "iccassandra", Password: "66a484add2298d0712f24c5014c43200"} //replace the username and password fields with their real settings.
+	//create a session on the cassandra server
 	session, err := cluster.CreateSession()
         if err != nil {
 		log.Println(err)
@@ -22,14 +27,15 @@ func main() {
 	}
 	defer session.Close()
 
-	// create keyspaces
+	// create keyspaces(namespace that defines data replication on node)and specify the network topolgy
+	//create a keyspace name sleep_centre
 	err = session.Query("CREATE KEYSPACE IF NOT EXISTS sleep_centre WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'AWS_VPC_US_WEST_2' : 3};").Exec()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	// create table
+	// create table with name sleep_study
 	err = session.Query("CREATE TABLE IF NOT EXISTS sleep_centre.sleep_study (name text, study_date date, sleep_time_hours float, PRIMARY KEY (name, study_date));").Exec()
 	if err != nil {
 		log.Println(err)
